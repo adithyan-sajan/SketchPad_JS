b3c6cd3d9d08ebcbb106dec2a3f81d89bc6ddaad
Adithyanarayanan <101406636+adithyan-sajan@users.noreply.github.com>
2025-03-24 09:49:59 +0530

    Add fixed sizing for canvas and rainbow mouseover

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-
 const container = document.querySelector(".mainContainer");
+const dimensionInput = document.querySelector("#dimensions");
+const createCanvasButton = document.querySelector(".canvasButton");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("box");
-    container.appendChild(cell);
+
+function chooseRandomColor() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    return `#${randomColor}`;
 }
+
+createCanvasButton.addEventListener("click", () => {
+    side = parseInt(dimensionInput.value);
+    if (side > 100) {
+        side = 100;
+        console.log("shrunk");
+    }
+    boxSize = containerSize / side;
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+        console.log("rem");
+    }
+    createCanvas(side, boxSize);
+});
+
+function createCanvas(side, size) {
+    for (let i = 0; i < side * side; i++) {
+
+        const cell = document.createElement("div");
+        cell.classList.add("box");
+
+        container.appendChild(cell);
+        cell.style.width = `${size}px`;
+        cell.style.height = `${size}px`;
+
+
+        cell.addEventListener("mouseover", () => {
+            // cell.classList.add("boxHovered");
+            cell.style.backgroundColor = chooseRandomColor();
+        });
+    }
+}
+
+size = 10;
+
+const containerSize = container.clientWidth;
+var boxSize = containerSize / size;
+createCanvas(size, boxSize);
+console.log(containerSize, boxSize);
